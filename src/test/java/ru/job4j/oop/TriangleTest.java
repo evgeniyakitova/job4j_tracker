49be9901939f703b7d114d6e39e46855c4748c07
evgeniyakitova <kitova.evgeniya.00@mail.ru>
2021-09-30 22:53:59 +0300

    2.2. Рефакторинг - Площадь треугольника. [#122642 #209895] Тесты

--- src/test/java/ru/job4j/oop/TriangleTest.java
+++ src/test/java/ru/job4j/oop/TriangleTest.java
@@ -1,18 +1,28 @@
 package ru.job4j.oop;
 
 import static org.junit.Assert.*;
-import static org.hamcrest.Matchers.closeTo;
+import static org.hamcrest.Matchers.*;
 import org.junit.Test;
 
 public class TriangleTest {
 
     @Test
-    public void area() {
+    public void whenExistThen8() {
         Point a = new Point(0, 0);
         Point b = new Point(4, 0);
         Point c = new Point(0, 4);
         Triangle triangle = new Triangle(a, b, c);
         double rsl = triangle.area();
         assertThat(rsl, closeTo(8, 0.001));
+    }
+
+    @Test
+    public void whenNotExist() {
+        Point a = new Point(2, 2);
+        Point b = new Point(4, 4);
+        Point c = new Point(0, 0);
+        Triangle triangle = new Triangle(a, b, c);
+        double rsl = triangle.area();
+        assertThat(rsl, is(-1.0));
     }
 }
