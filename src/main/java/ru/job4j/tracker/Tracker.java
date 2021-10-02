5daf0b2ffb19c0aff872e3a91a05f0a97ef1931b
evgeniyakitova <kitova.evgeniya.00@mail.ru>
2021-10-02 05:57:56 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,31 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] arrayOfKey = new Item[size];
+        int index = 0;
+        for (int i = 0; i < size; i++) {
+            Item item = items[i];
+            if (key.equals(item.getName())) {
+                arrayOfKey[index] = item;
+                index++;
+            }
+        }
+        return Arrays.copyOf(arrayOfKey, index);
+    }
+
+    public Item[] findAll() {
+        Item[] arrayWithoutNull = new Item[size];
+        int index = 0;
+        for (int i = 0; i < size; i++) {
+            Item item = items[i];
+            if (item != null) {
+                arrayWithoutNull[index] = item;
+                index++;
+            }
+        }
+        return Arrays.copyOf(arrayWithoutNull, index);
     }
 }
