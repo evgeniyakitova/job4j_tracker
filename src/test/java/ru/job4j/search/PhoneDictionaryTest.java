74f19f3582238ced47c8f9844f4bf30ad316a705
evgeniyakitova <kitova.evgeniya.00@mail.ru>
2021-10-18 14:10:31 +0300

    1. Телефонный справочник на базе ArrayList [#41598 #216464]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -17,4 +17,14 @@ public class PhoneDictionaryTest {
         ArrayList<Person> persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname(), is("Arsentev"));
     }
+
+    @Test
+    public void whenNotFind() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "555444", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Elena");
+        assertThat(persons.size(), is(0));
+    }
 }
