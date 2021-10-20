e0ce62595a93bcf1458710b12601757a6b1d6dbd
evgeniyakitova <kitova.evgeniya.00@mail.ru>
2021-10-20 17:21:10 +0300

    1. Паспорт и жители [#212656]

---
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class PassportOfficeTest {
+
+    @Test
+    public void whenAddThenTrue() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.get(citizen.getPassport()), is(citizen));
+    }
+
+    @Test
+    public void whenAddThenFalse() {
+        Citizen first = new Citizen("356913", "Alex");
+        Citizen second = new Citizen("356913", "Mike");
+        PassportOffice office = new PassportOffice();
+        office.add(first);
+        office.add(second);
+        assertThat(office.get(first.getPassport()), is(first));
+    }
+}
