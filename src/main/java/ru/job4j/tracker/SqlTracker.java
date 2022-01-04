f781b583bbc23e5d4560981f0375e48d8288e517
evgeniyakitova <kitova.evgeniya.00@mail.ru>
2022-01-04 15:14:45 +0300

    3. Liquibase. Интеграционные тесты. [#95881 #246644]

--- src/main/java/ru/job4j/tracker/SqlTracker.java
+++ src/main/java/ru/job4j/tracker/SqlTracker.java
@@ -9,6 +9,14 @@ import java.util.Properties;
 public class SqlTracker implements Store, AutoCloseable {
     private Connection cn;
 
+    public SqlTracker(Connection cn) {
+        this.cn = cn;
+    }
+
+    public SqlTracker() {
+
+    }
+
     public void init() {
         try (InputStream in = SqlTracker.class.getClassLoader().
                 getResourceAsStream("app.properties")) {
